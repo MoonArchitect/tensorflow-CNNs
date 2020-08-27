6c3561ca18daeea87f981ab9683923383fb97926
MoonArchitect <45274732+MoonArchitect@users.noreply.github.com>
2020-08-27 15:39:39 -0400

    Removed ShakeDrop and Mish

--- Layers.py
+++ Layers.py
@@ -1,54 +1,6 @@
 import tensorflow as tf
+import tensorflow.keras as nn
 from tensorflow.keras.backend import in_test_phase
-import tensorflow.keras as nn 
-
-
-class ShakeDrop(nn.layers.Layer):
-    def __init__(self, p = 1, alpha=(0, 0), beta=(0, 1), *kwargs):
-        super(ShakeDrop, self).__init__(kwargs)
-        self.p = tf.constant([p], dtype='float32', name='p')
-        self.alpha = tf.constant(alpha, dtype='float32', name='alpha')
-        self.beta = tf.constant(beta, dtype='float32', name='beta')
-        pass
-    
-    #@tf.function
-    @tf.custom_gradient
-    def _ShakeDrop(self, input):
-        b = tf.cond(tf.random.uniform([1], 0, 1)[0] <= self.p, lambda: tf.constant(1.0), lambda: tf.constant(0.0))
-        def grad(dy):
-            beta = tf.random.uniform([128, 1, 1, 1], self.beta[0], self.beta[1])
-            return (b + beta - beta * b) * dy
-        #alpha = tf.random.uniform([1], self.alpha[0], self.alpha[1])
-        #return input, grad
-        return tf.scalar_mul(b, input), grad
-        #in_test_phase((self.p + (self.alpha[1] + self.alpha[0]) * (1 - self.p) / 2)[0], (b + alpha - b*alpha)[0])
-
-    def call(self, input):
-        #return tf.cond(tf.random.uniform([1], 0, 1) <= self.p, lambda: tf.scalar_mul(tf.constant(1.0), input), lambda: tf.scalar_mul(tf.constant(0.0), input))
-        return self._ShakeDrop(input)
-
-
-class Mish(nn.layers.Layer):
-    def __init__(self, *kwargs):
-        super(Mish, self).__init__(kwargs)
-
-    @tf.function
-    @tf.custom_gradient
-    def MishFn(self, x):
-        
-        def grad(dy):
-            sp = tf.math.log1p(tf.exp(x)) if x < 20 else x
-            grad_sp = 1 - tf.exp(-sp)
-            tsp = tf.tanh(sp)
-            grad_tsp = (1 - tsp*tsp) * grad_sp
-            grad = x * grad_tsp + tsp
-            return dy * grad
-        
-        return x * tf.tanh(tf.math.softplus(x)), grad
-
-    def call(self, x):
-        return MishFn(x)#x * tf.tanh(tf.math.softplus(x))
-
 
 def PreActConv(filters,
                kernel_size,
