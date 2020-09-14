821eed4f787f4dd7d0bd5cbe2480b6577d1ef24d
MoonArchitect <45274732+MoonArchitect@users.noreply.github.com>
2020-09-14 19:27:34 -0400

    Create __init__.py

---
+++ Models/__init__.py
@@ -0,0 +1,17 @@
+from . import ResNetV2
+
+#from . import SeNetSD
+
+#from . import SeNet
+
+from . import ResNetSD
+
+from . import DenseNet
+
+from . import ResNeXt
+
+from . import ResNeXtSD
+
+from . import MobileNetV2
+
+#from . import MobileNetV3
