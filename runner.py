3af8306db3225ec6cdf510f447113f3e3a3e27db
MoonArchitect <45274732+MoonArchitect@users.noreply.github.com>
2021-09-04 16:31:07 -0400

    Improve code readability

--- runner.py
+++ runner.py
@@ -13,31 +13,6 @@ def parse_args():
     return parser.parse_args()
 
 
-def parse_flags(models, quotes = True):  # TODO swap double quotes for single quotes inside arguments
-    serialized = []
-
-    if not isinstance(models, list):
-        models = [ models ]
-
-    for model in models:
-        command = ""
-
-        for flag, value in model.items():
-            if isinstance(value, bool):
-                if value:
-                    command += f" --{flag}"
-            elif quotes and isinstance(value, str):
-                command += f" --{flag}=\"{value}\""
-            elif value is None:
-                pass
-            else:
-                command += f" --{flag}={value}"
-
-        serialized.append(command)
-        
-    return serialized
-
-
 def is_flag_valid(cfg, flag, overrides = False):
     flag_types = {
         "models": list,
@@ -63,38 +38,37 @@ def is_flag_valid(cfg, flag, overrides = False):
 
     assert cfg[flag] is None or isinstance(cfg[flag], flag_types[flag]), \
         f"Flag '{flag}' is of incorrect type '{type(cfg[flag])}, expected '{flag_types[flag]}'"
-    
-    return True
 
 
-def main():
-    # get file
-    args = parse_args()
-
+def parse_config(path):
     # read cfg
-    with open(args.cfg) as file:
+    with open(path) as file:
         cfg = yaml.load(file, Loader=yaml.FullLoader)
 
     # build all model configurations
-    models = []
+    configs = []
     if "models" not in cfg:
-        raise ValueError(f"models flag was not found in {args.cfg} -> cfg: {cfg}")
+        raise ValueError(f"models flag was not found in {path} -> cfg: {cfg}")
     
     base = {}
     for flag in cfg:
-        if flag == "models" or not is_flag_valid(cfg, flag):
+        if flag == "models":
             continue
+        is_flag_valid(cfg, flag)
         base[flag] = cfg[flag]
 
-    assert isinstance(cfg["models"], list), "Models should be a list of configurations"
+    assert isinstance(cfg["models"], list), "'models' should be a list of configurations"
     
     for config in cfg["models"]:
         overrides = base.copy()
-
         override_info = ""
+
         for flag in config:
-            if flag == "model" or not is_flag_valid(config, flag, True):
+            if flag == "model":
                 continue
+
+            is_flag_valid(config, flag, True)
+            
             if flag in overrides:
                 override_info += f"\tOverriding {flag} from {overrides[flag]} to {config[flag]}\n"
             overrides[flag] = config[flag]
@@ -104,56 +78,76 @@ def main():
             print(override_info)
             
         for name in list(config["model"]):
-            models.append( overrides.copy() )
-            models[-1]["model"] = name
-
+            configs.append( overrides.copy() )
+            configs[-1]["model"] = name
     
-    arguments = parse_flags(models)
-    clean_arguments = parse_flags(models, False)
+    return configs
 
-    # train -> if checkpoint is given, read,
-    # read progress file
-    # TODO check if yaml file
-    open(args.checkpoint, "a+").close()
-    with open(args.checkpoint, "r+") as file:
+
+def parse_flags(configs, quotes = True):  # TODO swap double quotes for single quotes inside arguments
+    serialized = []
+
+    if not isinstance(configs, list):
+        configs = [ configs ]
+
+    for model in configs:
+        command = ""
+
+        for flag, value in model.items():
+            if isinstance(value, bool):
+                if value:
+                    command += f" --{flag}"
+            elif quotes and isinstance(value, str):
+                command += f" --{flag}=\"{value}\""
+            elif value is None:
+                pass
+            else:
+                command += f" --{flag}={value}"
+
+        serialized.append(command)
+        
+    return serialized
+
+
+def parse_commands(configs, checkpoint_dir):
+    commands = []
+    arguments = parse_flags(configs)
+    
+    clean_arguments = parse_flags(configs, False)
+    ids = [ hashlib.md5(("train.py" + clean + f" --checkpoint={checkpoint_dir}").encode()).hexdigest() for clean in clean_arguments ]
+
+    open(checkpoint_dir, "a+").close()  # TODO check if yaml file
+    with open(checkpoint_dir, "r+") as file:
         checkpoint = yaml.load(file, Loader=yaml.FullLoader)
 
-    ids = [hashlib.md5(("train.py" + clean + f" --checkpoint={args.checkpoint}").encode()).hexdigest() for clean in clean_arguments]
+    for flags, id in zip(arguments, ids):
+        command = f'python train.py {flags} --checkpoint="{checkpoint_dir}"'
+        
+        if checkpoint is not None and id in checkpoint:
+            if checkpoint[id]["status"] == "Finished":  # TODO check if "status" is present
+                continue
+            
+            command += f" --id={id} --name={checkpoint[id]['cfg']['name']}"  # TODO check if fields exist
+        
+        commands.append(command)
+
+    return [ c + " --dryrun --full_info" for c in commands ] + commands
+
+
+def main():
+    args = parse_args()
+
+    configurations = parse_config(args.cfg)
     
-    for model, id in zip(arguments, ids):
-        command = "python train.py" + model + " --dryrun" + " --full_info"
-        
-        if checkpoint is not None and id in checkpoint:
-            if checkpoint[id]["status"] == "Finished":  # TODO check if "status" is present
-                continue
-            
-            command += f" --id={id} --name={checkpoint[id]['cfg']['name']}"  # TODO check if fields exist
-        
-        print(f"\n{'-'*50}  Dry run: {id}  {'-'*50}")
+    commands = parse_commands(configurations, args.checkpoint)
+    
+    for command in commands:
         print(f"Running:   {command}\n\n")
         
         os.system(command)
 
-        print(f"\n{'-'*150}\n")
+        print(f"\n{'-'*135}\n")
 
-    for model, id in zip(arguments, ids):
-        command = "python train.py" + model + f" --checkpoint=\"{args.checkpoint}\""
-        
-        if checkpoint is not None and id in checkpoint:
-            if checkpoint[id]["status"] == "Finished":  # TODO check if "status" is present
-                continue
-            
-            command += f" --id={id} --name={checkpoint[id]['cfg']['name']}"  # TODO check if fields exist
-
-        print(f"\n{'-'*300}")
-        print(f"Id:   {id}")
-        print(f"Running:   {command}\n\n")
-        
-        os.system(command)
-
-        print(f"\n{'-'*300}\n")  # TODO decrease
-
-    # print(cfg)
 
 
 if __name__ == "__main__":
